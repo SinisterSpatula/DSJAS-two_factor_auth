f57c291cb8fdb1a40b777ef6dafc90bdc3cb84ef
SinisterSpatula <49018201+SinisterSpatula@users.noreply.github.com>
2021-05-14 16:53:25 -0500

    Update content.js

--- global/content.js
+++ global/content.js
@@ -1,10 +1,8 @@
 console.log("Two Factor authentication login process module loaded");
-var randPhone
-if (localStorage.hasOwnProperty("dsjas2faphone")) {
-    let storerandPhone =  Math.floor(Math.random() * (8999) + 1000);
-    localStorage.setItem("dsjas2faphone", storerandPhone);
-    randPhone = storerandPhone;
-    console.log("Two Factor module: local storage key was empty generate new: " + storerandPhone);
+var randPhone = localStorage.getItem("dsjas2faphone");
+if (!randPhone) {
+    randPhone =  Math.floor(Math.random() * (8999) + 1000);
+    localStorage.setItem("dsjas2faphone", randPhone);
     }
 
 function wait(ms)
