112661723d295b16010b087e244fb9457dbc30df
SinisterSpatula <49018201+SinisterSpatula@users.noreply.github.com>
2021-05-14 15:10:29 -0500

    Update content.js

--- global/content.js
+++ global/content.js
@@ -1,6 +1,6 @@
 console.log("Two Factor authentication login process module loaded");
 var randPhone = localStorage.getItem("dsjas2faphone");
-if (randPhone === null) {
+if (randPhone == null) {
     let storerandPhone =  Math.floor(Math.random() * (8999) + 1000);
     localStorage.setItem("dsjas2faphone", storerandPhone);
     randPhone = storerandPhone;
