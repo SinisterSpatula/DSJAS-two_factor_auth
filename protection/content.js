d3e66aa8c58c062ccf20e126d3372f29ec10d2fc
SinisterSpatula <49018201+SinisterSpatula@users.noreply.github.com>
2021-05-14 13:25:21 -0500

    Update content.js

--- protection/content.js
+++ protection/content.js
@@ -3,27 +3,26 @@ function runProtection()
   console.log("Two Factor authentication showing wait");
   $("#protectionModalWait").modal();
   
-  setTimeout(function(){ $("#protectionModalWait").modal("hide"); }, 4000);
+  setTimeout(function(){ $("#protectionModalWait").modal("hide"); }, 3000);
   
   console.log("Two Factor authentication showing choice");
   document.getElementById("callme").innerHTML = "Call me at: (***) *** - " + randPhone;
   document.getElementById("textme").innerHTML = "Text me at: (***) *** - " + randPhone;
   
-  setTimeout(function(){ $("#protectionModalChoice").modal("show"); }, 5000);
+  setTimeout(function(){ $("#protectionModalChoice").modal(); }, 4000);
 
   return false;
 }
 
 function runInitialPrompt() {
   $("#protectionModalChoice").modal("hide");
-  $("#protectionModalWait").modal("show");
+  $("#protectionModalWait").modal();
   console.log("Two Factor authentication InitialPrompt showing wait again");
-
   
   /* Please enter your OTP code. */
-  $("#protectionModalWait").modal("hide");
+  setTimeout(function(){ $("#protectionModalWait").modal("hide"); }, 7000);
   console.log("Two Factor authentication Showing response");
-  $("#protectionModalResponse").modal("show");
+  setTimeout(function(){ $("#protectionModalResponse").modal(); }, 8000);
 }
 
 function runVerification() {
@@ -31,22 +30,19 @@ function runVerification() {
   /* continue the login or show help message. */
   if ((givenSolution.length > 5) && givenSolution.endsWith("0")) {
         $("#protectionModalResponse").modal("hide");
-        $("#protectionModalWait").modal("show");
+        $("#protectionModalWait").modal();
 
     
     
-        $("#protectionModalWait").modal("hide");
-        $('#loginForm').off();
-        $('#loginForm').submit();
-      }
+        setTimeout(function(){ $("#protectionModalWait").modal("hide"); $('#loginForm').off(); $('#loginForm').submit(); }, 3000);
   else {
     $("#protectionModalResponse").modal("hide");
-    $("#protectionModalWait").modal("show");
+    $("#protectionModalWait").modal();
 
     
     
-    $("#protectionModalWait").modal("hide");
-    $("#protectionModalHelp").modal("show");
+    setTimeout(function(){ $("#protectionModalWait").modal("hide"); }, 3000);
+    setTimeout(function(){ $("#protectionModalHelp").modal(); }, 4000);
   }
 }
 
